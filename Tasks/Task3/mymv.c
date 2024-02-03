a582442ed4e4c2b78683d23d7fdb5e925933dd48
moazragab12 <moazr8365@gmail.com>
2024-02-03 05:00:32 -0500

    edit Task_3

--- Tasks/Task3/mymv.c
+++ Tasks/Task3/mymv.c
@@ -6,23 +6,21 @@
 
 
 #define RET_STATUS 0
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  const char *src = argv[1];
-  const char *destination = argv[2];
+    const char *src = argv[1];
+    const char *destination = argv[2];
 
-  if (argc != 3)
-    {
-      printf ("Usage: %s file_soure  file_destination\n", argv[0]);
-      return -1;
+    if (argc != 3) {
+        printf("Usage: %s file_soure  file_destination\n", argv[0]);
+        return -1;
     }
 
-  if (rename (src, destination) != 0)
-    {
-      printf ("There is an error with your mv\n");
-      return -1;
+    if (rename(src, destination) != 0) {
+        printf("There is an error with your mv\n");
+        return -1;
     }
-  printf ("File moved Successfully from %s to %s \n", src, destination);
-  return RET_STATUS;
+    printf("File moved Successfully from %s to %s \n", src, destination);
+    return RET_STATUS;
 }
+
