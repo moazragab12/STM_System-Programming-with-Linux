77026c05e2b5df533961ad96d02fc2a1aeddcc7f
Moaz Ragab <123643446+moazragab12@users.noreply.github.com>
2024-02-09 16:40:26 +0200

    Adding Comments

--- Tasks/Task4/picoshell.c
+++ Tasks/Task4/picoshell.c
@@ -8,157 +8,137 @@
 #include <string.h>
 
 
-void
-tokenizeInput (char *input, char ***tokens, int *numArgs)
-{
-  *numArgs = 0;			// Initialize the number of arguments
+/*
+ * Function: tokenizeInput
+ * -------------------------
+ * Tokenizes the input string and stores the tokens in a dynamically allocated array.
+ *
+ * input: The input string to be tokenized.
+ * tokens: Pointer to a pointer where the tokens will be stored.
+ * numArgs: Pointer to an integer where the number of arguments will be stored.
+ */
+void tokenizeInput(char *input, char ***tokens, int *numArgs) {
+    *numArgs = 0;			// Initialize the number of arguments
+    char *token = strtok(input, " ");
+    *tokens = NULL;			// Initialize the array of pointers to NULL
 
-  char *token = strtok (input, " ");
-  *tokens = NULL;		// Initialize the array of pointers to NULL
+    // Tokenize the input string
+    while (token != NULL) {
+        // Allocate memory for the token array
+        *tokens = realloc(*tokens, (*numArgs + 1) * sizeof(char *));
+        if (*tokens == NULL) {
+            perror("realloc");
+            exit(EXIT_FAILURE);
+        }
 
-  while (token != NULL)
-    {
-      *tokens = realloc (*tokens, (*numArgs + 1) * sizeof (char *));
-      if (*tokens == NULL)
-	{
-	  perror ("realloc");
-	  exit (EXIT_FAILURE);
-	}
+        // Allocate memory for each token and copy it
+        (*tokens)[(*numArgs)++] = strdup(token);
 
-      (*tokens)[(*numArgs)++] = strdup (token);	// Allocate memory for each token
-
-      token = strtok (NULL, " ");
+        // Get the next token
+        token = strtok(NULL, " ");
     }
 
-  // Set the last argument to NULL to indicate the end
-  *tokens = realloc (*tokens, (*numArgs + 1) * sizeof (char *));
-  if (*tokens == NULL)
-    {
-      perror ("realloc");
-      exit (EXIT_FAILURE);
+    // Add a NULL pointer at the end of the token array to indicate the end
+    *tokens = realloc(*tokens, (*numArgs + 1) * sizeof(char *));
+    if (*tokens == NULL) {
+        perror("realloc");
+        exit(EXIT_FAILURE);
     }
-  (*tokens)[*numArgs] = NULL;
+    (*tokens)[*numArgs] = NULL;
 }
 
-int
-main (int argc, char *current_prog_argv[])
-{
-  int num_write = -1;
-  unsigned char buf[300];
 
-  char *cmd = NULL;
-  char **tokens;
-  int numArgs;
-  size_t n = 0;
-  while (1)
-    {
-      printf ("Hello brother lets start >> ");
-      int size = getline (&cmd, &n, stdin);
-      cmd[size - 1] = 0;
-      //handling empty input
-      if (strlen (cmd) == 0)
-	continue;
-      tokenizeInput (cmd, &tokens, &numArgs);	//tokenize the input
+/*
+ * Function: main
+ * --------------
+ * The main function of the program. Implements a simple shell.
+ */
+int main(int argc, char *current_prog_argv[]) {
+    int num_write = -1;
+    unsigned char buf[300];
+    char *cmd = NULL;
+    char **tokens;
+    int numArgs;
+    size_t n = 0;
 
+    // Main loop for the shell
+    while (1) {
+        printf("Hello brother lets start >> ");
 
-      if (strcmp ("exit", tokens[0]) == 0)
-	{
-	  printf ("GOOD BYE !\n ");
-	  break;
-	}
-      else if (strcmp ("echo", tokens[0]) == 0)
-	{
+        // Read input from the user
+        int size = getline(&cmd, &n, stdin);
+        cmd[size - 1] = 0;  // Remove newline character
 
-	  for (int i = 1; i < numArgs; i++)
-	    {
+        // Handling empty input
+        if (strlen(cmd) == 0)
+            continue;
 
-	      //1 refers to stdout
-	      int num_write_echo = write (1, tokens[i], strlen (tokens[i]));
-	      write (1, " ", 1);
+        // Tokenize the input command
+        tokenizeInput(cmd, &tokens, &numArgs);
 
-	      if (num_write_echo < 0)
-		{
+        // Execute commands based on the first token
+        if (strcmp("exit", tokens[0]) == 0) {
+            // Exit the shell
+            printf("GOOD BYE !\n ");
+            break;
+        } else if (strcmp("echo", tokens[0]) == 0) {
+            // Echo command implementation
+            for (int i = 1; i < numArgs; i++) {
+                // Write each token to stdout
+                int num_write_echo = write(1, tokens[i], strlen(tokens[i]));
+                write(1, " ", 1);
+                if (num_write_echo < 0) {
+                    printf("Could not write: Error\n");
+                }
+            }
+            printf("\n");
+        } else if (strcmp("pwd", tokens[0]) == 0) {
+            // Pwd command implementation
+            if (numArgs != 1) {
+                printf("Usage: %s\n", tokens[0]);
+            }
+            if (getcwd(buf, sizeof(buf)) == NULL) {
+                printf("The size of path is too long\n");
+            }
+            num_write = write(1, buf, strlen(buf));
+            if (num_write < 0) {
+                printf(">> Could not write to stdout\n");
+            }
+            printf("\n");
+        } else if (strcmp("cd", tokens[0]) == 0) {
+            // Cd command implementation
+            if (numArgs != 2) {
+                printf("Usage: %s Destination directory\n", tokens[0]);
+            }
+            if (chdir(tokens[1]) != 0) {
+                printf("There is an error in your cd\n");
+            }
+        } else {
+            // External command execution
+            printf("External command will be done\n");
+            pid_t returned_pid = fork();
+            if (returned_pid > 0) {
+                // Parent process waits for the child to complete
+                int wstatus;
+                wait(&wstatus);
+            } else if (returned_pid == 0) {
+                // Child process executes the command
+                execvp(tokens[0], tokens);
+                printf("I am not in the mode of execution. Exec failed\n");
+                return -1;
+            } else {
+                printf("ERROR: I could not get a child\n");
+            }
+        }
 
-		  // Handle error if write fails
-
-		  printf ("Couldnot write :Error");
-		}
-	    }
-
-	  printf ("\n");
-
-	}
-      else if (strcmp ("pwd", tokens[0]) == 0)
-	{
-
-	  if (numArgs != 1)
-	    {
-	      printf ("NO_ OF ARGUMINTS is wrong:%d\n", numArgs);
-	      printf ("Usage: %s\n", tokens[0]);
-
-	    }
-	  if (getcwd (buf, sizeof (buf)) == NULL)
-	    {
-	      printf ("The size of path is too way long");
-
-	    }
-
-	  num_write = write (1, buf, strlen (buf));
-	  if (num_write < 0)
-	    {
-	      printf (">> Could not write to stdout\n");
-
-	    }
-	  printf ("\n");
-
-	}
-      else if (strcmp ("cd", tokens[0]) == 0)
-	{
-
-	  if (numArgs != 2)
-	    {
-	      printf ("Usage: %s Destination directory\n", tokens[0]);
-
-	    }
-	  if (chdir (tokens[1]) != 0)
-	    {
-	      printf ("There is an error in your cd\n");
-
-	    }
-
-	}
-      else
-	{
-	  printf ("External command will be done\n");
-
-	  pid_t returned_pid = fork ();
-	  if (returned_pid > 0)
-	    {
-	      int wstatus;
-	      wait (&wstatus);
-	    }
-	  else if (returned_pid == 0)
-	    {
-	      execvp (tokens[0], tokens);
-	      printf ("I am not in the mode of execution. Exec failed\n");
-	      return -1;
-	    }
-	  else
-	    {
-	      printf ("ERROR: I could not get a child\n");
-	    }
-
-
-	}
-      free (cmd);
-      //  free the allocated memory
-      for (int i = 0; i < numArgs; ++i)
-	{
-	  free (tokens[i]);
-	}
-      free (tokens);		// Free the array of pointers
-      cmd = NULL;
-      n = 0;
+        // Free allocated memory
+        free(cmd);
+        for (int i = 0; i < numArgs; ++i) {
+            free(tokens[i]);
+        }
+        free(tokens);
+        cmd = NULL;
+        n = 0;
     }
-  return 0;
+    return 0;
 }
