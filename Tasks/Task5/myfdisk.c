9924e50afd65dd68695867f26bafafe7e5a24022
moazragab12 <moazr8365@gmail.com>
2024-02-21 08:10:09 -0500

    Fixed errors

--- Tasks/Task5/myfdisk.c
+++ Tasks/Task5/myfdisk.c
@@ -1,3 +1,5 @@
+#define _LARGEFILE64_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -37,23 +39,34 @@ void readPartitionTable(int fd, PartitionEntry * partitionTable)
 void printLogicalPartitions(int fd, uint32_t extended_lba,
 			    const char *device)
 {
-    lseek(fd, extended_lba * SECTOR_SIZE, SEEK_SET);
-    PartitionEntry logical_partition;
+    char buf[512];
+    lseek64(fd, (off64_t) extended_lba * SECTOR_SIZE, SEEK_SET);
     int logical_partition_number = 5;	// First logical partition number
-    while (read(fd, &logical_partition, sizeof(PartitionEntry)) > 0) {
-	if (logical_partition.partition_type != 0) {
-	    printf("%s%-5d %-10c %-10u %-10u %-10u %uG      %-10X\n",
+    read(fd, buf, SECTOR_SIZE);
+    PartitionEntry *logical_partition = (PartitionEntry *) & buf[446];
+    while (1) {
+	off64_t offsetofpartition =
+	    extended_lba + logical_partition[1].lba;
+	lseek64(fd, (off64_t) offsetofpartition * SECTOR_SIZE, SEEK_SET);
+	read(fd, buf, SECTOR_SIZE);
+	logical_partition = (PartitionEntry *) & buf[446];
+	{
+	    printf("%-8s%-4d  %-4c %-10lu %-10lu %-10u %06.2fG %5X\n",
 		   device,
 		   logical_partition_number,
 		   ' ',
-		   logical_partition.lba,
-		   logical_partition.lba + logical_partition.sector_count -
-		   1, logical_partition.sector_count,
-		   (uint32_t) (((uint64_t) logical_partition.sector_count *
-				512) / (1024 * 1024 * 1024)),
-		   logical_partition.partition_type);
+		   logical_partition->lba + offsetofpartition,
+		   logical_partition->lba +
+		   logical_partition->sector_count - 1 + offsetofpartition,
+		   logical_partition->sector_count,
+		   (double) ((uint64_t) logical_partition->sector_count *
+			     SECTOR_SIZE) / (1024 * 1024 * 1024),
+		   logical_partition->partition_type);
 	    logical_partition_number++;
 	}
+	if (logical_partition[1].lba == 0)
+	    break;
+
     }
 }
 
@@ -62,26 +75,29 @@ void printPartitionInfo(int fd, PartitionEntry * partitionTable,
 {
     printf("%-5s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "Device",
 	   "Boot", "Start", "End", "Sectors", "Size", "Id", "Type");
-    int logicalPartitionCount = 0;
+    int ext_flag = 5;
     for (int i = 0; i < 4; i++) {
-	if (partitionTable[i].partition_type == 0x05
-	    || partitionTable[i].partition_type == 0x0F) {
-	    // Logical partition, read extended partition table
-	    uint32_t extended_lba = partitionTable[i].lba;
-	    printLogicalPartitions(fd, extended_lba, device);
-	    logicalPartitionCount++;
-	} else if (partitionTable[i].partition_type != 0) {
-	    printf("%s%-5d %-10c %-10u %-10u %-10u %uG      %-10X\n",
+	if (partitionTable[i].partition_type == 5) {
+	    ext_flag = i;
+	}
+	if (partitionTable[i].sector_count != 0) {
+	    printf("%-8s%-4d  %-4c %-10u %-10u %-10u %06.2fG %5X\n",
 		   device,
-		   i + 1 - logicalPartitionCount,
+		   i + 1,
 		   partitionTable[i].status == 0x80 ? '*' : ' ',
 		   partitionTable[i].lba,
 		   partitionTable[i].lba + partitionTable[i].sector_count -
 		   1, partitionTable[i].sector_count,
-		   (uint32_t) (((uint64_t) partitionTable[i].sector_count *
-				512) / (1024 * 1024 * 1024)),
+		   (double) ((uint64_t) partitionTable[i].sector_count *
+			     SECTOR_SIZE) / (1024 * 1024 * 1024),
 		   partitionTable[i].partition_type);
 	}
+    }
+    if (ext_flag < 5) {
+	// Logical partition, read extended partition table
+	uint32_t extended_lba = partitionTable[ext_flag].lba;
+	printLogicalPartitions(fd, extended_lba, device);
+
     }
 }
 
